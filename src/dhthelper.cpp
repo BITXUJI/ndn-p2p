d6fbef1fff9812f67ad0a698e46ccfba93a158c3
ubuntu-ndn <bitxuji@gmail.com>
2020-01-14 01:02:49 -0800

    modified to a singleton file for dhtrunner

--- src/dhthelper.cpp
+++ src/dhthelper.cpp
@@ -6,20 +6,20 @@ DhtHelper::DhtHelper()
     m_mode->run(4222, dht::crypto::generateIdentity(), true);
 }
 
-void DhtHelper::putValue(const std::string &key, const dht::Value &value)
-{
-    m_node->put(key, std::move(value));
-}
+// void DhtHelper::putValue(const std::string &key, const dht::Value &value)
+// {
+//     m_node->put(key, std::move(value));
+// }
 
-void DhtHelper::getValue(const std::string &key)
-{
-    m_node->get(key,[](const std::vector<std::shared_ptr<dht::Value>>& values){
-        for (const auto &value : values)
-        {
-            std::cout << "Found value" << *value << std::endl;
-        }
-        //TODO 
-        m_store->insert({key, std::make_shared(values)}); 
-        return true;
-    }
-}
+// void DhtHelper::getValue(const std::string &key)
+// {
+//     m_node->get(key,[](const std::vector<std::shared_ptr<dht::Value>>& values){
+//         for (const auto &value : values)
+//         {
+//             std::cout << "Found value" << *value << std::endl;
+//         }
+//         //TODO 
+//         m_store->insert({key, std::make_shared(values)}); 
+//         return true;
+//     }
+// }
--- src/dhthelper.h
+++ src/dhthelper.h
@@ -29,10 +29,10 @@ public:
     {
         return m_node;
     }
-    void putValue(const std::string &key, const dht::Value &value);
-    void getValue(const std::string &key);
+    //void putValue(const std::string &key, const dht::Value &value);
+    //void getValue(const std::string &key);
 
 private:
     static dht::DhtRunner *m_node;
-    std::map<std::string, std::shared_ptr<std::vector<std::shared_ptr<dht::Value>>>> m_store;
+    //std::map<std::string, std::shared_ptr<std::vector<std::shared_ptr<dht::Value>>>> m_store;
 };
